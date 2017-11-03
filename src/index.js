d859179202a1e62d748874137bbada485ce0fc6b
Felix Volny <felix@volny.co>
2017-11-03 16:12:46 +0700

    .env file for absolute imports

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-import store, { history } from './store'
-import App from './containers/app'
-import registerServiceWorker from './registerServiceWorker'
+import store, { history } from 'store'
+import App from 'containers/app'
+import registerServiceWorker from 'registerServiceWorker'
 
-import './index.css'
+import 'index.css'
 
 const target = document.querySelector('#root')
 
