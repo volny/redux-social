eb63c9d01c4da648435a0f6329c1b94bf400329d
Felix Volny <felix@volny.co>
2017-11-03 16:34:12 +0700

    first component - react-headroom for navigation

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import Headroom from 'react-headroom'
 
-const App = () => <h1>Hello Main</h1>
+import Home from 'components/home'
+
+const App = () => (
+  <div>
+    <Headroom style={{ minHeight: '80px', background: '#e5e5e5' }}>
+      <Link to="/">Home</Link>
+    </Headroom>
+    <main>
+      <Route exact path="/" component={Home} />
+    </main>
+  </div>
+)
 
 export default App
