a328b95586f4e8aca106ca5ba725bf9975cc0661
Felix Volny <felix@volny.co>
2017-11-06 20:41:36 +0700

    dispatch auth actions

--- src/containers/AuthenticateContainer/index.js
+++ src/containers/AuthenticateContainer/index.js
@@ -1,33 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import Authenticate from 'components/Authenticate'
 import auth from 'helpers/auth'
+import * as userActions from 'modules/users'
 
-class AuthenticateContainer extends Component {
-  handleAuth() {
-    auth().then(user => {
-      console.log(user)
-    })
-  }
-  render() {
-    return (
-      <Authenticate isFetching={this.props.isFetching} error={this.props.error} onAuth={this.handleAuth} />
-    )
+const handleAuth = async ({ dispatch }) => {
+  dispatch(userActions.fetchingUser())
+  try {
+    const user = await auth()
+    dispatch(userActions.fetchingUserSuccess(user.uid, user, Date.now()))
+    dispatch(userActions.authUser(user.uid))
+  } catch (error) {
+    userActions.fetchingUserFailure(error)
   }
 }
+
+const AuthenticateContainer = props => (
+  <Authenticate isFetching={props.isFetching} error={props.error} onAuth={handleAuth.bind(null, props)} />
+)
 
 AuthenticateContainer.propTypes = {
   isFetching: PropTypes.bool.isRequired,
   error: PropTypes.string.isRequired,
 }
 
-// https://stackoverflow.com/questions/34211076/destructuring-deep-properties
 const mapStateToProps = ({ users: { isFetching, error } }) => ({
   isFetching,
   error,
 })
+
+// const mapStateToProps = state => {
+//   console.log(state.users)
+//   return {
+//     isFetching: state.users.isFetching,
+//     error: state.users.error,
+//   }
+// }
 
 // https://github.com/reactjs/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
 export default connect(mapStateToProps)(AuthenticateContainer)
