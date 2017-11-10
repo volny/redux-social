70281365a9453ef4cc956794264e6c225eba897a
Felix Volny <felix@volny.co>
2017-11-10 23:19:34 +0700

    push to history with withRouter

--- src/containers/AuthenticateContainer/index.js
+++ src/containers/AuthenticateContainer/index.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 
 import Authenticate from 'components/Authenticate'
 import * as userActions from 'modules/users'
 
-const AuthenticateContainer = ({ isFetching, error, fetchAndHandleAuthedUser }) => (
-  <Authenticate isFetching={isFetching} error={error} onAuth={() => fetchAndHandleAuthedUser()} />
-)
+class AuthenticateContainer extends Component {
+  static propTypes = {
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+    fetchAndHandleAuthedUser: PropTypes.func.isRequired,
+    // withRouter
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+  }
 
-AuthenticateContainer.propTypes = {
-  isFetching: PropTypes.bool.isRequired,
-  error: PropTypes.string.isRequired,
-  fetchAndHandleAuthedUser: PropTypes.func.isRequired,
+  handleAuth = async () => {
+    await this.props.fetchAndHandleAuthedUser()
+    this.props.history.push('/feed')
+  }
+
+  render() {
+    return (
+      <Authenticate isFetching={this.props.isFetching} error={this.props.error} onAuth={this.handleAuth} />
+    )
+  }
 }
 
-const mapStateToProps = ({ users: { isFetching, error } }) => ({
-  isFetching,
-  error,
-})
-
+const mapStateToProps = ({ users: { isFetching, error } }) => ({ isFetching, error })
 const mapDispatchToProps = dispatch => bindActionCreators(userActions, dispatch)
 
-export default connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer))
